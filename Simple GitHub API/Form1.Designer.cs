367b980bff0a2bb213d4e79a3d8c4ca41607bb8f
Harshil Darji <darjiharshil2994@gmail.com>
2016-06-05 00:31:07 +0530

    Initial commit

---
+++ Simple GitHub API/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Simple_GitHub_API
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TabStop = false;
+            this.textBox1.Text = "Enter GitHub User Name";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(197, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(11, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "User Info:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DimGray;
+            this.label3.Location = new System.Drawing.Point(12, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 307);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simple GitHub API";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Simple GitHub API/Form1.cs
@@ -0,0 +1,62 @@
+﻿using Octokit;
+using System.Windows.Forms;
+
+namespace Simple_GitHub_API
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.MouseDown += TextBox1_MouseDown;
+        }
+
+        private void TextBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            textBox1.Clear();
+            textBox1.ForeColor = System.Drawing.Color.Black;
+        }
+
+        private async void button1_Click(object sender, System.EventArgs e)
+        {
+            string uName = textBox1.Text;
+            if (uName.Equals("") || uName.Equals("Enter GitHub User Name"))
+            {
+                textBox1.ForeColor = System.Drawing.Color.Red;
+                label2.Text = "Error:";
+                label1.Text = "Please enter user name.";
+            }
+            else
+            {
+                label2.Text = "User Info:";
+                label1.Text = "Please wait...";
+                try
+                {
+                    var github = new GitHubClient(new ProductHeaderValue("SimpleGitHubAPI"));
+                    var user = await github.User.Get(uName);
+                    label3.Text = "Notes:"
+                        + "\n1. Date format: MM/DD/YYYY"
+                        + "\n2. Info not provided by the user will not be displayed."
+                        + "\n3. If E-mail ID of user is private, then if will not be displayed."
+                        + "\n4. If account type is Orgainization, \n     then value of followers and following may be zero.";
+                    label2.Text = user.Login + "'s Info:";
+                    label1.Text = "Name: " + user.Name
+                        + "\nSystem-wide ID: " + user.Id
+                        + "\nE-mail: " + user.Email
+                        + "\nAccount Type: " + user.Type
+                        + "\nFollowers: " + user.Followers
+                        + "\nFollowing: " + user.Following
+                        + "\nTotal Public Repos: " + user.PublicRepos
+                        + "\nTotal Private Repos: " + user.TotalPrivateRepos
+                        + "\nCreated at: " + user.CreatedAt;
+                }
+                catch
+                {
+                    label2.Text = "Error:";
+                    label1.Text = "1. Please check internet connection."
+                        + "\n2. Please check the user name.";
+                }
+            }
+        }
+    }
+}
